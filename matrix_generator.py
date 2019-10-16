d0de1a3d1ca6b96b7f4bd21f5033cf9110ad76ca
DrQuestion <alessio.albanese.bioinf@gmail.com>
2019-10-16 12:40:10 +0200

    Finished building matrix generator removing replicas

--- matrix_generator.py
+++ matrix_generator.py
@@ -5,20 +5,32 @@ sample_sheet_path = r"C:/Users/aless/Documents/DataMiningLab/gdc_sample_sheet.20
 matrix = pd.DataFrame()
 sample_no = 0
 file_to_barcode = dict()
+patients = set()
+patients_with_replicas = set()
 sheet_table = pd.read_csv(sample_sheet_path, sep="\t")
 for row_id, row in sheet_table.iterrows():
-    file_to_barcode[row["File Name"][:-3]] = row["Sample ID"]
+    barcode = row["Sample ID"]
+    file_to_barcode[row["File Name"][:-3]] = barcode
+    patient = barcode[8:-4]
+    if patient not in patients:
+        patients.add(barcode[8:-4])
+    else:
+        patients_with_replicas.add(patient)
 for folder in os.listdir(expression_data_path):
     files = os.listdir(os.path.join(expression_data_path, folder))
     for file in files:
         file_path = os.path.join(expression_data_path, folder, file)
         if file.endswith('.FPKM.txt'):
             barcode = file_to_barcode[file]
-            sample_expr = pd.read_csv(file_path, sep='\t')
-            if sample_no < 1:
-                matrix['Gene_ID'] = sample_expr.iloc[:, 0]
-                matrix[barcode] = sample_expr.iloc[:, 1]
-                sample_no = 1
+            if barcode[8:-4] not in patients_with_replicas:
+                sample_expr = pd.read_csv(file_path, sep='\t')
+                if sample_no < 1:
+                    matrix['Gene_ID'] = sample_expr.iloc[:, 0]
+                    matrix[barcode] = sample_expr.iloc[:, 1]
+                    sample_no = 1
+                else:
+                    matrix[barcode] = sample_expr.iloc[:, 1]
             else:
-                matrix[barcode] = sample_expr.iloc[:, 1]
-print(matrix)
+                continue
+matrix = matrix.set_index('Gene_ID')
+matrix.to_csv(r"C:/Users/aless/Documents/DataMiningLab/expressionMatrix.tsv", sep='\t')
