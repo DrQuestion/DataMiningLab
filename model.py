d870a0c28fe3466b6266c6bd277611f480e896f7
Viet Nhat Nguyen <nguyenvietnhat95@gmail.com>
2019-12-03 11:11:29 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,14 +1,50 @@
 import pandas
 import numpy as np
+import sklearn
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold, GridSearchCV
 
-"""
-    Select a subset of table with certain genes
-    parameters:
+
+def select_table(table, sets):
+    """ Select a subset of table with certain genes
+    Args:
         table: pandas dataframe with rows are samples, columns are genes
         sets: list of sets in which each set contains interested genes
-"""
-def select_table(table, sets):
+    Returns:
+        subset table
+    """
+    
     union_set = {}
     for s in sets:
         union_set = union_set.union(s)
     return table[list(union_set)]
+    
+
+
+def training(X_train, y_train):
+    """ Perform cross_validation on SVM
+    Args:
+        X_train: input data, expected to has numpy format with rows are samples, columns are genes
+        y_train: output
+    Returns:
+        Trained model
+    """
+
+    possible_parameters = {
+        'C': [1e1, 1e2, 1e3],
+        'gamma': [1e0, 1e-1, 1e-2]
+    }
+    svc = SVC(kernel='rbf')
+    
+    # Perform grid search cross validation
+    clf = GridSearchCV(svc, possible_parameters, n_jobs=8, cv=3)
+    clf.fit(X_train, y_train)
+    print(clf.best_params_)
+    
+    # Train with best setting to get scores on cross validation
+    kf = KFold(n_splits=3, shuffle=True)
+    scores = sklearn.model_selection.cross_validate(clf.best_estimator_, X_train, y_train, cv=kf, scoring=['accuracy', 'f1', 'precision', 'recall'])
+    for key in scores:
+        print(key, np.mean(scores[key]))
+        
+    return clf.best_estimator_
