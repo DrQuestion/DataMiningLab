5d969d8416c884eaeed3a8f9ce15d7004471d856
rfloc95 <r.scandino@gmail.com>
2019-12-03 23:19:53 +0100

    Objective generator, no indivumed data and filters for dead and alive > 2years

--- objective_generator.py
+++ objective_generator.py
@@ -8,14 +8,13 @@ for i, row in clinical.iterrows():
   #code for filtering Invidumed samples(with barcodes AA and AG)
   barcode = row[1].split("-")
   if barcode[1] != "AA" and barcode[1] != "AG":
-     # Filter for dead and alive > 5years
+  # Filter for dead and alive > 2years
     if row[8] == "Dead" and row[9] != "--":
       rows.append(i)
     else:
       if row[28] != "--" and int(row[28]) > 2*365: #calculated in days and not in years (days_to_last_followup)
         rows.append(i)
 
-#print("len of i", len(rows))
 clinical = clinical.iloc[rows]
 over2years = []
 for i, row in clinical.iterrows():
@@ -24,6 +23,5 @@ for i, row in clinical.iterrows():
   else:
     over2years.append(1)
 
-#print(len(over2years))
 objective = pd.DataFrame( list(zip(clinical.iloc[:,1], over2years)), columns=["barcode", "over2years"])
 objective.to_csv("objective.tsv", sep="\t")
