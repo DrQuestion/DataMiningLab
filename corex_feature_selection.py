a36356187c83f9362ef06ec5ec07d02ade4e3622
DrQuestion <alessio.albanese.bioinf@gmail.com>
2019-12-04 00:41:38 +0100

    Finished writing down corex feature selection

--- corex_feature_selection.py
+++ corex_feature_selection.py
@@ -4,11 +4,13 @@ import matrix_generator as mg
 
 N_FEATURES = mg.FINAL_NUMBER_OF_FEATURES
 expression_matrix_path = f'/home/alessio.albanese/exprMatTop{N_FEATURES}ByVar.tsv'
-#expression_matrix_path = rf"C:/Users/aless/Documents/UniTn/DataMiningLab/exprMatTop{N_FEATURES}ByVar.tsv"
+# expression_matrix_path = rf"C:/Users/aless/Documents/UniTn/DataMiningLab/exprMatTop{N_FEATURES}ByVar.tsv"
 output_clusters_path = f'/home/alessio.albanese/corex_clusters.tsv'
-#output_clusters_path = rf"C:/Users/aless/Documents/UniTn/DataMiningLab/corex_clusters.tsv"
+# output_clusters_path = rf"C:/Users/aless/Documents/UniTn/DataMiningLab/corex_clusters.tsv"
+output_custers_tcs_path = f'/home/alessio.albanese/corex_clusters_tcs.tsv'
+# output_custers_tcs_path = rf"C:/Users/aless/Documents/UniTn/DataMiningLab/corex_clusters_tcs.tsv"
 REPS = 5
-N_CPU = 50
+N_CPU = 100
 RAM = 200
 
 expression_matrix = pd.read_csv(expression_matrix_path, sep='\t', index_col=0, header=0)
@@ -19,6 +21,8 @@ layer1 = ce.Corex(n_hidden=200, dim_hidden=3, marginal_description='gaussian', s
 clusters = layer1.clusters
 clusters_table = pd.DataFrame(list(zip(expression_matrix.index.values, clusters)), columns=['gene_ids', 'clusters'])
 clusters_table.to_csv(output_clusters_path, sep='\t')
+clusters_tcs = pd.DataFrame(list(zip(range(200), layer1.tcs)), columns=['clusters', 'tcs'])
+clusters_tcs.to_csv(output_custers_tcs_path, sep='\t')
 
 
 
