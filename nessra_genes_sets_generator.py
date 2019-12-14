934dcff5715160ed48ee931f3e20b562f1bb5f2e
rfloc95 <r.scandino@gmail.com>
2019-12-14 17:22:38 +0100

    returns list of sets of ensembl genes

--- nessra_genes_sets_generator.py
+++ nessra_genes_sets_generator.py
@@ -1,61 +1,95 @@
-import os
-import re
 import numpy as np
 import pandas as pd
+import mygene
 
-def setGen(exp_file, int_file, freq_tres, n_genes):
+
+
+def tcodeSymbolDictGen(tcode_mat):
+    '''
+    Takes tcode matrix and creates a dictionary of tcode to ENS
+    '''
+    tcode_symbol_dict = {}
+    print("I'm importing tcode-to-Symbol dictionary, this will take a minute")
+    for i, row in tcode_mat.iterrows():
+        tcode_symbol_dict[row[0].lower()] = row[10]
+    print("Done, dictionary ready")
+    
+    return tcode_symbol_dict
+
+def tcodeToEns(t_gene, tcodeSymbol_dict):
+    '''
+    Takes tcode_gene and dictionary tcode to symbol, returns ens using package mygene!
+    '''
+    symbol_gene = tcodeSymbol_dict[t_gene]
+
+    return symbol_gene
+
+
+def setGen(exp_file, int_file, freq_tres, n_genes, tcode_symbol_dict):
     '''
     Takes as input expansion ordered list of genes, related interaction file, frequency treshold for filter, max number of genes per file
     Returns a set of genes
     '''
-    gene_set = set()
-
-    # Take genes from expansion file with filter treshold for frequency
+  
+    symbol_genes = []
+    # Take genes from expansion file with filter treshold for frequency and converts them into Symbol
     for i, gene in exp_file.iterrows():
-        if len(gene_set) < n_genes:
-            if gene[1] not in gene_set and gene[3] > freq_tres:
-                gene_set.add(gene[1])
+        if len(symbol_genes) < n_genes:
+            symbol_gene = tcodeToEns(gene[1],  tcode_symbol_dict)
+            if symbol_gene not in symbol_genes and gene[3] > freq_tres:
+                symbol_genes.append(symbol_gene)
         else:
             break    
-
+    
     # Check for final length, if not n_genes look into interactions
-    if len(gene_set) < n_genes:
+    if len(symbol_genes) < n_genes:
         for i, ints in int_file.iterrows():
-            if len(gene_set) < n_genes and ints[4] > freq_tres:
+            if len(symbol_genes) < n_genes and ints[4] > freq_tres:
                 # check first x position
-                if ints[1] in gene_set:
-                    if ints[2] not in gene_set:
-                        gene_set.add(ints[2])
+                gen_posX = tcodeToEns(ints[1],  tcode_symbol_dict)
+                gen_posY = tcodeToEns(ints[2],  tcode_symbol_dict)
+                if gen_posX in symbol_genes:
+                    if gen_posY not in symbol_genes:
+                        symbol_genes.append(gen_posY)
                 # eventually check y position
-                elif ints[2] in gene_set:
-                    if ints[1] not in gene_set:
-                        gene_set.add(ints[1])
+                elif gen_posY in symbol_genes:
+                    if gen_posX not in symbol_genes:
+                        symbol_genes.append(gen_posX)
             else:
                 break
-    
+
+    # Convert symbol genes into ens and add them into set, return set
+    gene_set = set()
+    mg = mygene.MyGeneInfo()
+    ens_genes = mg.querymany(symbol_genes, scopes='symbol', fields='ensembl.gene', species='human')
+    for el in ens_genes:
+        if "ensembl" in el:
+            if len(el["ensembl"]) > 1:
+                for gene in el["ensembl"]:
+                    gene_set.add(gene["gene"])
+            else:
+                gene_set.add(el["ensembl"]["gene"])
+        
     return gene_set
 
 
-# Imports all file from working directory and divides them into expansion and interaction files
-folder_files = os.listdir('./')
-exp_files = [name for name in folder_files if re.search('expansion', name)]
-int_files = [name for name in folder_files if re.search('interaction', name)]
 
+list_79 = [pd.read_csv('160679_Hs.expansion', sep = ",", header=1),
+            pd.read_csv('160679_Hs.interactions', sep = ",", header=1)]
+list_80 = [pd.read_csv('160680_Hs.expansion', sep = ",", header=1),
+            pd.read_csv('160680_Hs.interactions', sep = ",", header=1)]
+tcode_mat = pd.read_csv('hgnc_filtered_anno.csv', sep = ",", header=1)
 
 # Set filter tresholds and max genes per set here!
 freq_treshold = 0.3
 n_genes_per_file = 40
+tcode_symbol_dict = tcodeSymbolDictGen(tcode_mat)
 
-list_of_sets = []
-for i in range(len(exp_files)):
-    exp_genes = pd.read_csv(exp_files[i], sep = ",", header=1)
-    int_genes = pd.read_csv(int_files[i], sep = ",", header=1)
-    # apply function
-    my_set = setGen(exp_genes, int_genes, freq_treshold, n_genes_per_file)
-    print("looking at files {} and {}".format(exp_files[i], int_files[i]))
-    print("len of gene set: {}".format( len(my_set)))
-    print("")
-    list_of_sets.append(my_set)
+
+set79 = setGen(list_79[0], list_79[1], freq_treshold, n_genes_per_file, tcode_symbol_dict)
+set80 = setGen(list_80[0], list_80[1], freq_treshold, n_genes_per_file, tcode_symbol_dict)
+
+list_of_sets = [set79, set80]
 
 
 
